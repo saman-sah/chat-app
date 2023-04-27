21c9360dd2ac4578ef3e21193ddf088d57ebc438
SamanSah <sahraei.saman91@gmail.com>
2023-04-27 17:56:16 +0430

    dynamic pages

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -1,19 +1,27 @@
 <template>
     <div class="contacts">
-        <div v-for="(user, index) in users" :key="index" class="contact">
-            <div class="pic danvers">
-                <h1>{{user.name.charAt(0)}}</h1>
-            </div>
-            <div class="badge" :style="{'background-color': user.online ? '#38d938' : 'red'}"></div>
-            <div class="name user-name-link">
-                <router-link to="/chat-page">
-                    <h4>{{ user.name }}</h4>
-                </router-link>
-            </div>
-            <div class="badge-status" :style="{'background-color': user.online ? '#38d938' : 'red'}">
-                <span>{{ user.online ? 'Online' : 'Offline'}}</span>
+        <div class="pages-nav">
+            <div class="name">
+                Contacts
             </div>
         </div>
+        <div class="contacts-item">
+            <div v-for="(user, index) in users" :key="index" class="contact">
+                <div class="pic danvers">
+                    <h1>{{user.name.charAt(0)}}</h1>
+                </div>
+                <div class="badge" :style="{'background-color': user.online ? '#38d938' : 'red'}"></div>
+                <div class="name user-name-link">
+                    <router-link to="/chat-page">
+                        <h4>{{ user.name }}</h4>
+                    </router-link>
+                </div>
+                <div class="badge-status" :style="{'background-color': user.online ? '#38d938' : 'red'}">
+                    <span>{{ user.online ? 'Online' : 'Offline'}}</span>
+                </div>
+            </div>
+        </div>
+       
     </div>
 </template>
 
@@ -44,6 +52,8 @@ export default {
 </script>
 
 <style>
-
+.contacts-item {
+    padding: 1em;
+}
 
 </style>
